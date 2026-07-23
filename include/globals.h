4146e44b065facdc784560e2abb226194a4b1143
Limkon <130622161+Limkon@users.noreply.github.com>
2026-07-23 10:51:32 +0800

    Update globals.h

--- include/globals.h
+++ include/globals.h
@@ -4,7 +4,6 @@
 #include <windows.h>
 #include <stdbool.h>
 
-// UI 控件 ID
 #define ID_BTN_APPLY      1001
 #define ID_EDIT_MIN       1002
 #define ID_EDIT_MAX       1003
@@ -17,14 +16,13 @@
 #define ID_CMB_HK_RECORD  1010
 #define ID_CMB_HK_PLAY    1011
 
-// 全局热键 ID
 #define HOTKEY_ID_TOGGLE   2001
 #define HOTKEY_ID_STOP     2002
 #define HOTKEY_ID_BIND     2003
 #define HOTKEY_ID_RECORD   2004
 #define HOTKEY_ID_PLAYBACK 2005
 
-#define MAX_RECORD_SIZE   1024
+#define MAX_RECORD_SIZE 1024
 
 typedef struct {
     POINT pt;
@@ -32,16 +30,12 @@ typedef struct {
     DWORD delay;
 } MouseRecord;
 
-// 全局变量
 extern bool is_active;
-extern int interval_min;
-extern int interval_max;
+extern int interval_min, interval_max;
 extern HWND hStatusLabel;
 
-extern int action_button;
-extern int action_mode;
-extern int hotkey_toggle;
-extern int hotkey_stop;
+extern int action_button, action_mode;
+extern int hotkey_toggle, hotkey_stop;
 
 extern HWND target_hwnd;
 extern POINT bind_pt;
@@ -52,7 +46,6 @@ extern MouseRecord record_buffer[MAX_RECORD_SIZE];
 extern int record_count;
 extern bool is_recording;
 extern int playback_count;
-extern int hotkey_record;
-extern int hotkey_playback;
+extern int hotkey_record, hotkey_playback;
 
 #endif
